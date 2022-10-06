ca5b63ae38fd69e2f600f488bdc1fe8e95c0fe14
andylamax <andylamax@programmer.net>
2022-10-06 10:35:51 +0300

    Added settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+pluginManagement {
+    enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    dependencyResolutionManagement {
+        versionCatalogs {
+            file("gradle/versions").listFiles().map {
+                it.nameWithoutExtension to it.absolutePath
+            }.forEach { (name, path) ->
+                create(name) { from(files(path)) }
+            }
+        }
+    }
+}
+
+fun includeRoot(name: String, path: String) {
+    include(":$name")
+    project(":$name").projectDir = File(path)
+}
+
+fun includeSubs(base: String, path: String = base, vararg subs: String) {
+    subs.forEach {
+        include(":$base-$it")
+        project(":$base-$it").projectDir = File("$path/$it")
+    }
+}
+
+val tmp = 1
+
+rootProject.name = "asoft"
+
+// dependencies
+includeSubs("functions", "../functions", "core")
+includeSubs("expect", "../expect", "core", "coroutines")
+includeSubs("koncurrent-primitives", "../koncurrent/primitives", "core", "coroutines", "mock")
+includeSubs("live", "../live", "core")
+
+// subprojects
+includeSubs("events", ".", "core", "inmemory", "react")
+
